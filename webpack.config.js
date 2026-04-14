7c8c3536c345662c8ebf8a2855a85f6f1f10211b
Sébastien VIDAL <sebastien.vidal.dev@gmail.com>
2026-04-14 23:38:08 +0200

    standardize tools

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+// deps
+
+	// natives
+	const { join } = require("node:path");
+
+  // externals
+  const TerserPlugin = require("terser-webpack-plugin");
+
+// consts
+
+  const PUBLIC = join(__dirname, "public");
+
+// module
+
+module.exports = {
+
+  "mode": "development",
+
+  "entry": join(PUBLIC, "src", "index.tsx"),
+
+  "output": {
+    "filename": "bundle.min.js",
+    "path": join(PUBLIC, "dist")
+  },
+
+  "devtool": "source-map",
+
+  "module": {
+    "rules": [
+      {
+        "test": /\.tsx?$/,
+        "exclude": [ /node_modules/ ],
+        "use": [
+          {
+              "loader": "ts-loader",
+              "options": {
+                "configFile": join(__dirname, "tsconfig-front.json")
+              }
+          }
+        ]
+      }
+    ]
+  },
+  "optimization": {
+    "minimize": true,
+    "minimizer": [
+      new TerserPlugin({
+        "parallel": true,
+        "terserOptions": {
+          "format": {
+            "comments": false
+          }
+        },
+        "extractComments": false
+      })
+    ]
+  },
+
+  "resolve": {
+    "extensions": [ ".tsx", ".ts", ".js" ],
+  }
+
+};
