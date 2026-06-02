31e00f1ab28903c5fb5fd9213bfd17bfad407ad6
Sébastien VIDAL <sebastien.vidal.dev@gmail.com>
2026-06-02 19:01:50 +0200

    front : parsing response correction

--- public/src/SDK.ts
+++ public/src/SDK.ts
@@ -44,6 +44,44 @@ export class SDK extends EventEmitter<{
 
     }
 
+    // protected methods
+
+    protected _parseResponse (res: Response): Promise<unknown> {
+
+        if (res.ok) {
+
+            return new Promise((resolve: (content: unknown) => void, reject: (error: Error) => void): void => {
+
+                res.text().then((content: string): void => {
+
+                    try {
+                        return resolve(JSON.parse(content));
+                    }
+                    catch (e: unknown) { // eslint-disable-line @typescript-eslint/no-unused-vars
+                        return resolve(content);
+                    }
+
+                }).catch((err: Error): void => {
+                    console.warn(err);
+                    return reject(new Error("Impossible to parse response"));
+                });
+
+            });
+
+        }
+
+        return new Promise((resolve: unknown, reject: (error: Error) => void): void => {
+
+            res.json().then((content: components["schemas"]["Error"]): void => {
+                return reject(new Error(content.message));
+            }).catch((): void => {
+                return reject(new Error("Problem with request, has status '" + res.status + "' (" + res.statusText + ")"));
+            });
+
+        });
+
+    }
+
     // public methods
 
     public connect (): void {
@@ -99,8 +137,7 @@ export class SDK extends EventEmitter<{
 
             const parsedMessage: tEvents = JSON.parse(event.data) as tEvents;
 
-            // must disable the rule because the plugin name can be sended by another plugin
-            if ("mia-vlc-read-sound" === parsedMessage.plugin) { // eslint-disable-line @typescript-eslint/no-unnecessary-condition
+            if ("mia-vlc-read-sound" === parsedMessage.plugin as string) { // must be forced string type to avoid useless type error
 
                 switch (parsedMessage.command) {
 
@@ -160,19 +197,7 @@ export class SDK extends EventEmitter<{
             }
         }).then((res: Response): Promise<operations["getPluginDescriptor"]["responses"]["200"]["content"]["application/json"]> => {
 
-            if (res.ok) {
-                return res.json();
-            }
-
-            return new Promise((resolve: unknown, reject: (error: Error) => void): void => {
-
-                res.json().then((content: operations["getPluginDescriptor"]["responses"]["default"]["content"]["application/json"]): void => {
-                    return reject(new Error(content.message));
-                }).catch((): void => {
-                    return reject(new Error("Problem with request getPluginDescriptor has status '" + res.status + "' (" + res.statusText + ")"));
-                });
-
-            });
+            return this._parseResponse(res) as Promise<operations["getPluginDescriptor"]["responses"]["200"]["content"]["application/json"]>;
 
         });
 
@@ -190,22 +215,11 @@ export class SDK extends EventEmitter<{
             }
         }).then((res: Response): Promise<operations["getPluginStatus"]["responses"]["200"]["content"]["application/json"]> => {
 
-            if (res.ok) {
-                return res.json();
-            }
-            else if (404 === res.status) {
+            if (404 === res.status) {
                 return Promise.resolve("RELEASED");
             }
 
-            return new Promise((resolve: unknown, reject: (error: Error) => void): void => {
-
-                res.json().then((content: operations["getPluginStatus"]["responses"]["default"]["content"]["application/json"]): void => {
-                    return reject(new Error(content.message));
-                }).catch((): void => {
-                    return reject(new Error("Problem with request getPluginStatus has status '" + res.status + "' (" + res.statusText + ")"));
-                });
-
-            });
+            return this._parseResponse(res) as Promise<operations["getPluginStatus"]["responses"]["200"]["content"]["application/json"]>;
 
         });
 
